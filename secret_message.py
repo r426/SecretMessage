47e1243dd64cdd24a87858620e1dd6272a1fca2b
Your Name <rutaslv@hotmail.lt>
2020-07-25 17:43:55 +0300

    refactor the sequence of steps

--- secret_message.py
+++ secret_message.py
@@ -1,5 +1,10 @@
 import os
 
+# Create a new directory for the deciphered code
+dir = os.path.join("hack_code_done")
+if not os.path.exists(dir):
+    os.mkdir(dir)
+
 # List all files in a directory using os.listdir
 basepath = 'hack_code/'
 for fileName in os.listdir(basepath):
