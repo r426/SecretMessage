f446612f0bf175bb29a39ad36018356239127d4a
Your Name <rutaslv@hotmail.lt>
2020-07-27 22:41:26 +0300

    check if secret message directory exists

--- secret_message.py
+++ secret_message.py
@@ -3,8 +3,23 @@
 import os
 import shutil
 
+secretMessageDirectory = 'hack_code'
+crackedMessageDirectory = 'hack_code_done'
+
+try:
+    assert os.path.isdir(secretMessageDirectory), "Secret message directory does not exist."
+except AssertionError as object:
+    print(object)
+    exit()
+
+try:
+    assert not os.path.isdir(crackedMessageDirectory), "Outdated directory deleted."
+except AssertionError as object:
+    print(object)
+    shutil.rmtree(crackedMessageDirectory)
+
 # Copy the directory for deciphering the code
-shutil.copytree('hack_code', 'hack_code_done')
+shutil.copytree(secretMessageDirectory, crackedMessageDirectory)
 
 # List all files in a directory using os.listdir
 basepath = 'hack_code/'
